501a1327c2a3b03d421d8e5423678df5aa70f6b1
Shimi9999 <moguranian999@gmail.com>
2024-10-02 20:23:49 +0900

    Real initial commit

---
+++ fonthas.go
@@ -0,0 +1,108 @@
+// ttfフォントが対応していない文字をCSVの文字列から検出する
+// 1行目のデータを各カラム名として扱う
+
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/golang/freetype/truetype"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		fmt.Println("Usage: fonthas <ttf path> <csv path>")
+		os.Exit(1)
+	}
+	ttfPath := flag.Arg(0)
+	ttfData, err := loadTtf(ttfPath)
+	if err != nil {
+		panic(err)
+	}
+
+	csvPath := flag.Arg(1)
+	csvData, err := loadCsv(csvPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var columnName []string
+	for i := 0; i < len(csvData); i++ {
+		if i == 0 {
+			columnName = make([]string, len(csvData[i]))
+			copy(columnName, csvData[i])
+			continue
+		}
+		if len(csvData[i]) <= 1 {
+			continue
+		}
+
+		for j := 0; j < len(csvData[i]) && j < len(columnName); j++ {
+			str := csvData[i][j]
+			for _, rv := range str {
+				if ttfData.Index(rv) == 0 {
+					fmt.Printf("%d[%s]: %c in %s\n", i, columnName[j], rv, str)
+				}
+			}
+		}
+	}
+}
+
+func loadTtf(path string) (*truetype.Font, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	ttfBytes, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	ft, err := truetype.Parse(ttfBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return ft, nil
+}
+
+const BUFSIZE = 1024
+
+func loadCsv(path string) ([][]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("invalid csv file path: " + err.Error())
+	}
+	defer file.Close()
+
+	var csvStr string
+	buf := make([]byte, BUFSIZE)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("CSV file read error: " + err.Error())
+		}
+
+		csvStr += string(buf[:n])
+	}
+
+	r := csv.NewReader(strings.NewReader(csvStr))
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("CSV string read error: " + err.Error())
+	}
+
+	return records, nil
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module font
+
+go 1.17
+
+require github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+
+require golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
